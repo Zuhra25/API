d583f840b8c4ed196ed8a6e375f3b3cf7ad2df1b
ZU <zshaihova@gmail.com>
2023-09-29 13:18:24 +0300

    начальный коммит

---
+++ src/main/java/ru/hogwarts/schoolCreateAPI/SchoolCreateApiApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolCreateAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCreateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolCreateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/schoolCreateAPI/SchoolCreateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolCreateAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCreateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
